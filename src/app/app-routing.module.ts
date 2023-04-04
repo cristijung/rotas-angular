c7ddc9df147b5b6b86906a247a3e1bcc1950268b
Cristina Jung <cristijung@gmail.com>
2023-04-04 15:24:05 -0300

    inicial

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContatoComponent } from './contato/contato.component';
+import { ForumComponent } from './forum/forum.component';
+import { GaleriaComponent } from './galeria/galeria.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'contato', component: ContatoComponent},
+  {path: 'forum', component: ForumComponent},
+  {path: 'galeria', component: GaleriaComponent},
+  {path: '**', component: NotFoundComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { GaleriaComponent } from './galeria/galeria.component';
+import { ForumComponent } from './forum/forum.component';
+import { ContatoComponent } from './contato/contato.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    GaleriaComponent,
+    ForumComponent,
+    ContatoComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
